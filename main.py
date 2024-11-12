98a920946b6963638d66b8c8b2b97201ead3540a
Kacper Zieliński <zielinski_kacper@outlook.com>
2024-11-12 19:04:45 +0100

    Add add_reservation function

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 seats = [None, None, "Kacper", "Jan", None, None, "Aleksandra", None, None, "Wioletta"] #Przykładowa lista do sprawdzania, czy dana funkcja działa prawidłowo
+
 #Funkcja, która pozwala na wyświetlenie aktualnego stanu rezerwacji miejsc
 def print_seats(seats:list):
     for i in range(len(seats)):
@@ -6,4 +7,24 @@ def print_seats(seats:list):
             print(f"Miejsce nr {i + 1} jest wolne")
         else:
             print(f"Miejsce nr {i + 1} jest już zarezerwowane przez {seats[i]}")
+
+#Funkcja, która pozwala na dodanie nowej rezerwacji
+def add_reservation(seats:list):
+    name = input("Podaj swoje imię: ")
+    try:
+        seat_number = int(input("Podaj numer miejsca, które chcesz zarezerwować (miejsce musi być wolne): "))
+    except ValueError:
+        print("Wprowadzono nieprawidłową wartość!")
+        exit()
+    if seat_number - 1 < 0 or seat_number - 1 > len(seats):
+        print("Wprowadzono nieprawidłową wartość!")
+        exit()
+    elif seats[seat_number - 1] != None:
+        print("Wybrane miejsce jest już zajęte!")
+        exit()
+    else:
+        seats[seat_number - 1] = name
+        print("Rezerwacja zakończona pomyślnie!")
+print_seats(seats)
+add_reservation(seats)
 print_seats(seats)
